85111e9b027390f56dfa073956fd9c4b7d624d62
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-02 19:31:49 -0300

    Atualização e criação de novas rotas
    1. Alteração da rota de atualização dos dados do usuário
    2. Criação das rotas do tweet

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserController from './controllers/UserController';
 import AuthController from './controllers/AuthController';
+import Tweetcontroller from './controllers/TweetController';
 
 import AuthMiddleware from './middleware/authenticate';
 
@@ -11,6 +12,11 @@ routes.post('/signup', UserController.create);
 routes.post('/login', AuthController.login);
 
 routes.use(AuthMiddleware.autenticate);
-routes.post('/update', UserController.update);
+routes.post('/user/update', UserController.update);
+
+routes.get('/tweets', Tweetcontroller.index);
+routes.post('/tweets', Tweetcontroller.create);
+routes.put('/tweets/:tweetId', Tweetcontroller.update);
+routes.delete('/tweets/:tweetId', Tweetcontroller.delete);
 
 export default routes;

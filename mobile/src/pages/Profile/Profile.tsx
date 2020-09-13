ae5eb64b978402fba62f6fbc3555bec31db79ec2
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-13 12:51:21 -0300

    Botão de logout na página de Profile

--- mobile/src/pages/Profile/Profile.tsx
+++ mobile/src/pages/Profile/Profile.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Button } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import { Container, Text } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 const Profile: React.FC = () => {
+  const navigation = useNavigation();
+
+  const Logout = async () => {
+    await AsyncStorage.removeItem('@token');
+    navigation.reset({ routes: [{ name: 'Login' }] });
+  }
+
   return (
     <Container>
       <Text>Profile Page</Text>
+      <Button
+        onPress={Logout}
+        title="Logout"
+        color="#4bb0ee"
+        accessibilityLabel="Learn more about this purple button"
+      />
     </Container>
   );
 }

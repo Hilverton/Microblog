d4576d66aeda93e5f1befb47bf61ed251d101bd4
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-13 13:36:43 -0300

    Criação do componente Tweet, que mostra os tweets criados

---
+++ mobile/src/components/Tweet/Tweet.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Author, Content } from './styles';
+
+interface TweetProps {
+  tweet: Tweet;
+}
+
+const Tweet: React.FC<TweetProps> = ({ tweet }) => {
+  return (
+    <Container>
+      <Author>{tweet.name}</Author>
+      <Content>{tweet.content}</Content>
+    </Container>
+  );
+}
+export { Tweet };
---
+++ mobile/src/components/Tweet/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import colors from '../../styles';
+
+export const Container = styled.View`
+  padding: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Author = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Content = styled.Text`
+  font-size: 14px;
+  line-height: 20px;
+  margin: 10px 0;
+`;

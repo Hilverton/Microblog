a5779c024e8bdfc82a48837884a5e348245ab2e2
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-09-16 20:00:17 -0300

    Adição do avatar do usuário

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,6 +4,7 @@ export interface UserInfo extends Document {
 	name: string;
 	password: string;
 	email: string;
+	avatar?: string;
 }
 
 const UserSchema = new mongoose.Schema({
@@ -22,6 +23,10 @@ const UserSchema = new mongoose.Schema({
     	lowercase: true,
 		required: 'O campo email é obrigatório, não pode ser vazio.',
 		match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Preencha um email válido.']
+	},
+	avatar: {
+		type: String,
+		default: '',
 	}
 }, {
 	timestamps: true,

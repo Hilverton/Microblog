3aa313fd76c1d7b972cf7689cf9ac4d4b82363e6
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-10-28 13:05:02 -0300

    Refresh token implementado na página Preload

--- mobile/src/pages/Preload/Preload.tsx
+++ mobile/src/pages/Preload/Preload.tsx
@@ -1,34 +1,42 @@
-import React, { useEffect } from 'react';
-import { Container, Logo, LoadingIcon } from './styles';
-import { useNavigation } from '@react-navigation/native';
-import colors from '../../styles';
-import AsyncStorage from '@react-native-community/async-storage';
+import React, { useEffect } from "react";
+import { Container, Logo, LoadingIcon } from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { apiClient } from "../../services/api";
+import colors from "../../styles";
+import AsyncStorage from "@react-native-community/async-storage";
 
-import logoTwitter from '../../../assets/splashTwitter.png';
+import logoTwitter from "../../../assets/splashTwitter.png";
 
 const Preload: React.FC = () => {
   const navigation = useNavigation();
   useEffect(() => {
     const verifyToken = async () => {
       try {
-        const token = await AsyncStorage.getItem('@token');
-        if(token) navigation.reset({ routes: [{ name: 'PrivateRoutes' }] });
-        else navigation.navigate('Login');
+        const token = await AsyncStorage.getItem("@token");
+        if (!token) return navigation.navigate("Login");
+        const config = {
+          headers: { Authorization: `Bearer ${token}` },
+        };
+        const response = await apiClient.get("/refresh-token", config);
+        if (response.data.token) {
+          await AsyncStorage.setItem("@token", response.data.token);
+          navigation.reset({ routes: [{ name: "PrivateRoutes" }] });
+        }
       } catch (error) {
         console.log(error);
+        navigation.navigate("Login");
       }
-    }
+    };
 
     verifyToken();
-
   }, []);
 
   return (
     <Container>
       <Logo source={logoTwitter} />
-      <LoadingIcon size='large' color={colors.primary} />
+      <LoadingIcon size="large" color={colors.primary} />
     </Container>
   );
-}
+};
 
 export { Preload };
